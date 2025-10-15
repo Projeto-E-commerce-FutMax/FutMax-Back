4ecbb74c56fa34c504079cef79a9fd55dde2a8a3
RochaGR <gutop.rocha@gmail.com>
2025-10-15 13:09:07 -0300

    Feat: Iniciando implementação do Sistema de Login

---
+++ src/main/java/com/trier/futmax/dto/request/AuthRequestDTO.java
@@ -0,0 +1,18 @@
+package com.trier.futmax.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AuthRequestDTO {
+    private String nmEmail;
+    private String nmSenha;
+
+}
